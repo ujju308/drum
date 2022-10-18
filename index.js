de14d4eb0dd0526c4497bf80a9115b72a8f073df
ujju308 <114446853+ujju308@users.noreply.github.com>
2022-10-19 01:38:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.addEventListener("keypress",function(event){
 function makeSound(key){
     switch(key){
         case "w":
-           var audio = new Audio("tom-1.mp3");
+           var audio = new Audio("https://github.com/ujju308/drum/raw/main/crash.mp3");
            audio.play();
            break;
         case "a":
